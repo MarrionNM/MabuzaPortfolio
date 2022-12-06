9ca81780682f5ab02cf1baae8cb712632c736f80
Admin <77marrion@gmail.com>
2022-12-07 00:46:01 +0200

    final change

--- src/urls.py
+++ src/urls.py
@@ -5,9 +5,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.view.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("dashboard.urls", namespace='dashboard'))
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
