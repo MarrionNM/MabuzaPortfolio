9b2dd87b72fa0aed79bc4ecc2e07667719191343
Admin <77marrion@gmail.com>
2022-12-09 10:04:20 +0200

    merge psql with railway

--- src/settings.py
+++ src/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'src.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'devmabuza',
+        'NAME': 'railway',
         'USER' : 'postgres',
-        'PASSWORD' : 'admin',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
+        'PASSWORD' : 'HwhoQfcBAUzhMNtniSRl',
+        'HOST' : 'containers-us-west-142.railway.app',
+        'PORT' : '7415',
     }
     # 'ENGINE': 'django.db.backends.sqlite3',
     # 'NAME': BASE_DIR / 'db.sqlite3',
